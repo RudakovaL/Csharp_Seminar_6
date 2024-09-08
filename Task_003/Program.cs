097abf3eae8aa7d251e4c1c857175c853f093390
liubov <Madamlub98@yandex.ru>
2024-09-08 12:10:40 +0300

    Добавили решение задачи 3

---
+++ Task_003/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+
+        string cleanedInput = input.Replace(" ", "").ToLower();
+
+        char[] charArray = cleanedInput.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedInput = new string(charArray);
+
+        if (cleanedInput == reversedInput)
+        {
+            Console.WriteLine("Строка является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом.");
+        }
+    }
+}
+
